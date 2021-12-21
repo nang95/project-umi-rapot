30dd962d0cfb946c2321002c9fbc156081ee9f5e
danang9595 <rookie95cookies@gmail.com>
2021-12-21 12:43:58 +0700

    fix

--- resources/views/apps/guru/rapor/cetak-nilai.blade.php
+++ resources/views/apps/guru/rapor/cetak-nilai.blade.php
@@ -33,19 +33,27 @@
         
         <table style="width: 100%;margin-top: 10px; margin-bottom: 10px" border="2" cellspacing="0" cellpadding="10">
             <tr>
-                <td>No</td>
                 <td>Mata Pelajaran</td>
                 <td>Nilai</td>
                 <td>Predikat</td>
             </tr>
+            @php
+                $nr = 0;   
+            @endphp
             @foreach ($rapor as $item)
+            @php
+                $nr += $item->getTotalNilai($item->siswa_id, $item->guruMataPelajaran->mata_pelajaran_id);
+            @endphp
             <tr>
-                <td>{{ $loop->iteration }}</td>
                 <td>{{ $item->guruMataPelajaran->mataPelajaran->nama }}</td>
                 <td>{{ $item->getTotalNilai($item->siswa_id, $item->guruMataPelajaran->mata_pelajaran_id) }}</td>
                 <td>{{ $item->getPredikat($item->getTotalNilai($item->siswa_id, $item->guruMataPelajaran->mata_pelajaran_id)) }}</td>
             </tr>
             @endforeach
+            <tr>
+                <td>Nilai Rata-Rata</td>
+                <td colspan="2">{{ round($nr / $rapor->count()) }}</td>
+            </tr>
         </table>
 
         <table style="width: 100%;margin-top: 10px; margin-bottom: 10px" border="1" cellspacing="0" cellpadding="10">
